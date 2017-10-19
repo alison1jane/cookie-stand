75c78f608174d042d8ef87ab3dc26d3c7cd45d23
Jane Smith <alison.mcirvin@gmail.com>
2017-10-18 19:40:21 -0700

    issues with adding daily total cookies sold-for each location

--- app.js
+++ app.js
@@ -19,24 +19,66 @@ function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour
   allLocations.push(this);
   this.calcRandCustByHour();
   this.calcCookiesSoldByHour();
-  // this.tableRowMaker();
-  console.log('this.cookiesSoldByHour', this.cookiesSoldByHour);
+  this.tableRowMaker();
+  this.dailyCookiesSold = [];
+  this.dailyTotalCookiesSold = 0;
+  // console.log('this.cookiesSoldByHour', this.cookiesSoldByHour);
 }
 // console.log('***** ', this),
 //method to calculate random customers by hour
 //prototype code allows function below, access to the constructor above(and all  the properties and methods)
 //the constructor and any instantiations will have access to the prototpe functions
+
 MakeLocation.prototype.calcRandCustByHour = function() {
   for(var i = 0; i < hours.length; i++) {
     this.randCustByHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
   };
 };
+
 MakeLocation.prototype.calcCookiesSoldByHour = function() {
   for(var j = 0; j < hours.length; j++) {
-    this.cookiesSoldByHour.push(Math.round(this.avgCookieSoldPerHour * this.randCustByHour[j]));
+    var randomCookies = Math.round(this.avgCookieSoldPerHour * this.randCustByHour[j]);
+    this.cookiesSoldByHour.push(randomCookies);
+    this.dailyTotalCookiesSold = this.dailyTotalCookiesSold + randomCookies;
+    console.log('daily total cookies sold ' + ths.dailyTotalCookiesSold);
     console.log('this.cookiesSoldByHour inside calcCookiesSoldByHour:  ', this.cookiesSoldByHour);
   }
 };
+MakeLocation.prototype.tableRowMaker = function() {
+  var cookiestands = document.getElementById('cookiestands');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+
+  for(var i = 0; i < hours.length; i++) {
+    var tdEl = document.createElement('td');
+    //need cookies sold per hour
+    tdEl.textContent = this.cookiesSoldByHour[i];
+    console.log('this ', this);
+    trEl.appendChild(tdEl);
+  }
+  cookiestands.appendChild(trEl);
+};
+//Prototype for totals daily cookies sold
+MakeLocation.prototype.dailyCookiesSold = function() {
+  var cookiestands = document.getElementById('cookiestands');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.dailyCookiesSold;
+  trEl.appendChild(tdEl);
+
+  for(var i = 0; i < hours.length; i++) {
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesSoldByHour[i];
+    trEl.appendChild(tdEl);
+  }
+  var totalTdEl = document.createElement('td');
+  totalTdEl.textContent = this.dailyTotalCookiesSold;
+  trEl.appendChild(totalTdEl);
+  cookiestands.appendChild(trEl);
+};
+
 // function render() {
 //   var name = document.getElementById('cookiestands');
 //   console.log('name: ', name);
@@ -55,14 +97,16 @@ MakeLocation.prototype.calcCookiesSoldByHour = function() {
 //   }
 // };
 //make new loation for all 5 stores replaces object literal code, YAY!!!
-function makeStands() {
-  var firstAndPike = new MakeLocation ('First and Pike', 23, 65, 6.3);
-  var SeaTacAirport = new MakeLocation('SeaTac Airport', 3, 24, 1.2);
-  var seattleCenter = new MakeLocation('Seattle Center', 11, 38, 3.7);
-  var capitolHill = new MakeLocation('Capitol Hill', 20, 38, 2.3);
-  var Alki = new MakeLocation('Alki', 2, 16, 4.6);
-};
-makeStands();
+// function makeStands() {
+makeHeaderRow();
+var firstAndPike = new MakeLocation ('First and Pike', 23, 65, 6.3);
+var SeaTacAirport = new MakeLocation('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new MakeLocation('Seattle Center', 11, 38, 3.7);
+var capitolHill = new MakeLocation('Capitol Hill', 20, 38, 2.3);
+var Alki = new MakeLocation('Alki', 2, 16, 4.6);
+console.log(allLocations);
+// };
+// makeStands();
 //Create Table function
 //header row and table data
 //add table id in html
@@ -81,26 +125,6 @@ function makeHeaderRow() {
   cookiestands.appendChild(trEl);
 
 }
-makeHeaderRow();
-
-//create table rows
-MakeLocation.prototype.tableRowMaker = function() {
-  var cookiestands = document.getElementById('cookiestands');
-  var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
-  tdEl.textContent = 'this.name';
-  trEl.appendChild(tdEl);
-
-  for(var i = 0; i < hours.length; i++) {
-    var tdEl = document.createElement('td');
-    //need cookies sold per hour
-    tdEl.textContent = 'this.cookiesSoldByHour[i]';
-    trEl.appendChild(tdEl);
-  }
-  cookiestands.appendChild(trEl);
-};
-
-MakeLocation.prototype.tableRowMaker();
 
 //First and Pike Location
 // var firstAndPike = {
